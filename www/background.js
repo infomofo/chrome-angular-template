5e4356ee09011b08681a45841655305545126aa7
Will Chiong <wchiong@gilt.com>
2014-02-17 00:28:22 -0500

    remove plugins from version control

--- www/background.js
+++ www/background.js
@@ -6,7 +6,7 @@
  */
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
-    width: 244,
-    height: 380,
+    width: 320,
+    height: 550
   });
 });
